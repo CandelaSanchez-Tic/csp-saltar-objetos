80aa900e36068d292c3fdd5de56f988df2e90ca9
CandelaSanchez-Tic <135613777+CandelaSanchez-Tic@users.noreply.github.com>
2023-06-05 13:58:22 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,14 @@
 input.onButtonPressed(Button.A, function () {
     jugador_arrib.change(LedSpriteProperty.Y, -1)
     jugador_abajo.change(LedSpriteProperty.Y, -1)
-    basic.pause(500)
+    basic.pause(1000)
+    jugador_arrib.change(LedSpriteProperty.Y, 1)
+    jugador_abajo.change(LedSpriteProperty.Y, 1)
+})
+input.onButtonPressed(Button.B, function () {
+    jugador_arrib.change(LedSpriteProperty.Y, -1)
+    jugador_abajo.change(LedSpriteProperty.Y, -1)
+    basic.pause(1000)
     jugador_arrib.change(LedSpriteProperty.Y, 1)
     jugador_abajo.change(LedSpriteProperty.Y, 1)
 })
@@ -12,8 +19,15 @@ jugador_abajo = game.createSprite(1, 4)
 let obstaculo = game.createSprite(4, 4)
 basic.forever(function () {
     obstaculo.change(LedSpriteProperty.X, -1)
-    basic.pause(500)
+    basic.pause(750)
     if (obstaculo.get(LedSpriteProperty.X) == 0) {
         obstaculo.set(LedSpriteProperty.X, 4)
+        obstaculo.set(LedSpriteProperty.Y, randint(3, 4))
+    }
+    if (obstaculo.isTouching(jugador_abajo)) {
+        game.gameOver()
+    }
+    if (obstaculo.isTouching(jugador_arrib)) {
+        game.gameOver()
     }
 })
